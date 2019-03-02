b2e6e107d6601421570382d519d98b196ac2d076
superkapar <kapceru@gmail.com>
2019-03-02 13:49:09 +0100

    Zmiana

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("pleprlpelfpl");
         for (int i = 0; i < 100; i++) {
             System.out.println("to jest numer:" + i);
 
