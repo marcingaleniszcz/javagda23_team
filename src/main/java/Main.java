d6383b163c04a8faa6847042ec01068f90c0841e
Marcin Galeniszcz <marcingaleniszcz@gmail.com>
2019-03-02 13:33:02 +0100

    Poprawka

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ public class Main {
         for (int i = 0; i < 100; i++) {
             System.out.println("to jest numer:" + i);
 
+            System.out.println("kurwa to dziala");
+
         }
     }
 }
