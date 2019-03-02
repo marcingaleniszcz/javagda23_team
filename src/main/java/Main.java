43fbedaf46c9aef39de221e8985504dbe059f9b3
Marcin Galeniszcz <marcingaleniszcz@gmail.com>
2019-03-02 14:03:02 +0100

    uzgodniona

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("pleprlpelfpl");
+
         for (int i = 0; i < 69; i++) {                  // tu zmienilem xD
             System.out.println("cos dopisuje os1" + i);
+
 
         }
         System.out.println("Cośtam bylo nowego");
